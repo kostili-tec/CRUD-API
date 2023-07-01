9fd101cc9371b5dca38ed476c3f8088ba3d58b62
kostili-tec <weh4teyou@gmail.com>
2023-07-01 16:58:19 +0300

    feat: add server side error

--- src/index.ts
+++ src/index.ts
@@ -11,26 +11,34 @@ import { users } from './data/users.js';
 const PORT = process.env.PORT || 5001;
 
 const server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
-  const myRequest: IRequest = Object.assign(req, { users: users });
-  console.log(myRequest.url);
-  switch (myRequest.method) {
-    case 'GET':
-      getRequest(myRequest, res);
-      break;
-    case 'POST':
-      postRequest(myRequest, res);
-      break;
-    case 'PUT':
-      putRequest(myRequest, res);
-      break;
-    case 'DELETE':
-      deleteRequest(myRequest, res);
-      break;
-    default:
-      res.statusCode = 404;
-      res.setHeader('Content-Type', 'application/json');
-      res.write(JSON.stringify({ title: 'Not found', message: 'Route not found' }));
-      res.end();
+  try {
+    const myRequest: IRequest = Object.assign(req, { users: users });
+    console.log(myRequest.url);
+    if (myRequest.url === '/error') throw new Error('error');
+    switch (myRequest.method) {
+      case 'GET':
+        getRequest(myRequest, res);
+        break;
+      case 'POST':
+        postRequest(myRequest, res);
+        break;
+      case 'PUT':
+        putRequest(myRequest, res);
+        break;
+      case 'DELETE':
+        deleteRequest(myRequest, res);
+        break;
+      default:
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'application/json');
+        res.write(JSON.stringify({ title: 'Not found', message: 'Route not found' }));
+        res.end();
+    }
+  } catch (error) {
+    console.error('Server side error:', error);
+    res.statusCode = 500;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({ error: 'Server side error' }));
   }
 });
 
